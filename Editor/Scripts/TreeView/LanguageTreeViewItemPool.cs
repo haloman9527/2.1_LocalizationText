4fd0e9e99389ff2c5dbeb231bcea337c75c93414
HalfLobsterMan <766439226@qq.com>
2021-05-13 18:32:43 +0800

    update

--- Editor/Scripts/TreeView/LanguageTreeViewItemPool.cs
+++ Editor/Scripts/TreeView/LanguageTreeViewItemPool.cs
@@ -4,15 +4,9 @@ namespace CZToolKit.LocalizationText.Editor
 {
     public class LanguageTreeViewItemPool : PoolBase<LanguageTreeViewItem>
     {
-        /// <summary> 回收所有 </summary>
-        public override void RecycleAll()
+        protected override LanguageTreeViewItem CreateNewUnit()
         {
-            IdleList.AddRange(WorkList);
-            WorkList.Clear();
+            return new LanguageTreeViewItem();
         }
-
-        protected override LanguageTreeViewItem CreateNewUnit() { return new LanguageTreeViewItem(); }
-
-        protected override void OnRecycle(LanguageTreeViewItem unit) { base.OnRecycle(unit); }
     }
 }
